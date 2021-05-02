40fb89dc6ee82f34f8254dfc8816c3b0aa92cdcc
Marek Bedkowski <marek@bedkowski.pl>
2021-05-02 17:44:28 +0200

    Moving linuxbuild file into docker-compose.

--- linuxbuild.sh
+++
@@ -1,7 +0,0 @@
-docker run -it --rm \
-    -v "$PWD":/opt/app  \
-    -v "$HOME"/.m2:/root/.m2 \
-    softinstigate/graalvm-maven \
-    clean package -Pnative-image
-
-docker build -t function-sample-aws-native . -f Dockerfile-native

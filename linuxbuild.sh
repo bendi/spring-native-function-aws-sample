c04f9ef81ff7100bdf188c01a27280b9bca44b49
Marek Bedkowski <marek@bedkowski.pl>
2021-05-02 14:51:50 +0200

    Added ability to test lambda using docker.

--- linuxbuild.sh
+++ linuxbuild.sh
@@ -3,3 +3,5 @@ docker run -it --rm \
     -v "$HOME"/.m2:/root/.m2 \
     softinstigate/graalvm-maven \
     clean package -Pnative-image
+
+docker build -t function-sample-aws-native . -f Dockerfile-native
